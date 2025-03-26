642375bed2af26e7129ddedd598351ab5022152f
Steve Bauman <steven_bauman@outlook.com>
2025-03-26 18:21:48 -0400

    WIP

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -37,7 +37,7 @@ function Header() {
   return (
     <header
       className={clsx(
-        'sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none',
+        'sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 border-b border-slate-200 dark:border-slate-800 shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none',
         isScrolled
           ? 'dark:bg-slate-900/95 dark:backdrop-blur-sm dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75'
           : 'dark:bg-transparent',
@@ -76,10 +76,7 @@ export function Layout({ children }) {
       {isHomePage && <Hero />}
 
       <div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12">
-        <div className="hidden lg:relative lg:block lg:flex-none">
-          <div className="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
-          <div className="absolute top-16 right-0 bottom-0 hidden h-12 w-px bg-linear-to-t from-slate-800 dark:block" />
-          <div className="absolute top-28 right-0 bottom-0 hidden w-px bg-slate-800 dark:block" />
+        <div className="hidden lg:relative lg:block lg:flex-none border-r border-slate-200 dark:border-slate-800">
           <div className="sticky top-[4.75rem] -ml-0.5 h-[calc(100vh-4.75rem)] w-64 overflow-x-hidden overflow-y-auto py-16 pr-8 pl-0.5 xl:w-72 xl:pr-16">
             <Navigation />
           </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -29,49 +29,46 @@ export function Logo(props) {
   return (
     <svg
     xmlns="http://www.w3.org/2000/svg"
-    id="aaeheahc"
     width="289.8"
     height="61.732"
-    className="css-grg60t"
-    preserveAspectRatio="xMidYMid meet"
     viewBox="0 0 289.8 61.732"
     {...props}
   >
     <defs id="SvgjsDefs1327">
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
-      <linearGradient id="SvgjsLinearGradient1332">
+      <linearGradient id="gradient">
         <stop id="SvgjsStop1333" offset="0" stopColor="#ee2a7b"></stop>
         <stop id="SvgjsStop1334" offset="1" stopColor="#ff7db8"></stop>
       </linearGradient>
     </defs>
     <g
       id="SvgjsG1328"
-      fill='url("#SvgjsLinearGradient1332")'
+      fill='url("#gradient")'
       transform="matrix(.17677 0 0 .17677 -.354 -4.596)"
     >
       <g>
         <g
           xmlns="http://www.w3.org/2000/svg"
-          fill='url("#SvgjsLinearGradient1332")'
+          fill='url("#gradient")'
           fillRule="evenodd"
         >
           <path
@@ -82,7 +79,7 @@ export function Logo(props) {
     </g>
     <path
       id="SvgjsG1329"
-      fill="#000"
+      fill="currentColor"
       d="M4.4 20H1.7V6h2.7zm5.62 0H7.5V9.4h2.38v1.02h.04c.48-.74 1.46-1.32 2.56-1.32 1.12 0 2.2.58 2.7 1.4h.04c.54-.82 1.58-1.4 2.76-1.4 2.54 0 4.12 1.46 4.12 3.64V20h-2.38v-6.72c0-1.04-.58-1.78-1.84-1.78-1.18 0-1.82.68-1.82 1.64V20h-2.38v-6.72c0-1.02-.66-1.78-1.84-1.78-1.2 0-1.82.68-1.82 1.64zm23 0h-2.18v-1.02h-.04c0 .02-.74 1.32-2.88 1.32-2.56 0-3.82-1.38-3.82-3.14 0-1.84.98-2.66 3.6-3.3 1.22-.3 1.98-.52 2.28-.7s.46-.42.46-.74c0-.64-.6-1.12-1.8-1.12-1.42 0-1.9.68-2.24 1.74l-2.16-.9c.5-1.78 1.98-3.04 4.36-3.04q1.95 0 3.18 1.02c1.23 1.02 1.24 1.74 1.24 3.16zm-4.8-4.34c-1.34.32-1.62.76-1.62 1.32q0 1.08 1.56 1.08c1.58 0 2.36-.68 2.36-1.56v-1.84h-.04c-.06.1-.12.2-.18.26-.1.1-.8.42-2.08.74m10-2.06v2.18c0 1.22.9 2.18 2.28 2.18 1.52 0 2.6-1.16 2.6-3.26s-1.06-3.26-2.6-3.26c-1.38 0-2.28.96-2.28 2.16m0 10.9h-2.6V9.4h2.48v1.02h.04c0-.02.8-1.3 2.86-1.32 1.26 0 2.34.5 3.28 1.52.94 1 1.42 2.36 1.42 4.08 0 1.74-.46 3.1-1.36 4.1s-2 1.5-3.3 1.5c-1.6 0-2.46-.84-2.7-1.18l-.08-.14h-.04zM56.86 20H48V6h8.86v2.5H50.7v3.14h5.6v2.4h-5.6v3.46h6.16zm5.1 0h-2.6V9.4h2.5v1.02h.04c.58-.74 1.68-1.32 2.8-1.32 2.34 0 3.76 1.48 3.76 3.96V20h-2.6v-6.3c0-1.46-.5-2.2-1.86-2.2-1.28 0-2.04.8-2.04 2.02zm15.98-4.22V13.6c0-1.2-.9-2.16-2.28-2.16-1.54 0-2.6 1.16-2.6 3.26s1.08 3.26 2.6 3.26c1.38 0 2.28-.96 2.28-2.18m-6.72 6.38 1.94-1.34c.62 1.2 1.32 1.72 2.54 1.72 1.36 0 2.24-.68 2.24-2.22v-1.34h-.04c-.4.72-1.5 1.32-2.64 1.32-1.36 0-2.5-.5-3.42-1.48-.92-1-1.38-2.38-1.38-4.12 0-1.72.48-3.08 1.42-4.08.94-1.02 2.06-1.52 3.34-1.52 1.22 0 2.14.44 2.8 1.32h.04V9.4h2.48V20c0 1.54-.46 2.74-1.36 3.56s-2.06 1.24-3.48 1.24c-2.22 0-3.78-1.04-4.48-2.64M85.84 20h-2.6V9.4h2.6zm0-12.14h-2.6V5.4h2.6zM91.24 20h-2.6V9.4h2.5v1.02h.04c.58-.74 1.68-1.32 2.8-1.32 2.34 0 3.76 1.48 3.76 3.96V20h-2.6v-6.3c0-1.46-.5-2.2-1.86-2.2-1.28 0-2.04.8-2.04 2.02zm16.08-3.66 1.82 1.6c-1.08 1.58-2.54 2.36-4.38 2.36-1.48 0-2.68-.5-3.62-1.52s-1.4-2.38-1.4-4.08.46-3.04 1.38-4.06c.94-1.02 2.1-1.54 3.52-1.54s2.58.52 3.48 1.54c.92 1.02 1.38 2.36 1.38 4.06v.78h-7.12c.08 1.62.92 2.52 2.34 2.52 1.2 0 1.9-.64 2.6-1.66m-4.94-2.8h4.6c-.14-1.36-.96-2.16-2.34-2.16-1.28 0-2.16.94-2.26 2.16"
       transform="translate(86.85 2.991)scale(1.85343)"
     ></path>
